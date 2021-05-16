75cd53a43c1be9255342d816de2e813a9288e3d5
itsMO253 <mwarsame34@outlook.com>
2021-05-15 19:14:03 -0700

    added event lissteners

--- Drum Kit Project/index.js
+++ Drum Kit Project/index.js
@@ -1,1 +1,11 @@
-alert("IT WORKS");
+// grabs the length of how many buttons with the class drum
+var size = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i < size; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
+
+// what to do when click detected
+function handleClick() {
+    alert("I got clicked");
+}
